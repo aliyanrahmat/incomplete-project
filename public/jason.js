8541d6ed463a0d4304574b20bf8c4ff46595c140
shehzad <shehzadzahoor03@gmail.com>
2023-03-24 00:32:19 +0500

    incomplete

---
+++ public/jason.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var slideIndex = 0;
+    showSlides();
+  
+    function showSlides() {
+      var i;
+      var slides = $(".slide");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      slides[slideIndex-1].style.display = "block";
+      setTimeout(showSlides, 3000); // Change image every 3 seconds
+    }
+  });
+  
